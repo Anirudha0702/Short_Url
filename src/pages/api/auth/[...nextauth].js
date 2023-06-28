2c6a70f75f44b4b8185a9f73742294d53e6fbb28
Anirudha Pradhan <anirudhapradhan403@gmail.com>
2023-06-28 19:44:28 +0530

    env

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,13 +1,12 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from  "next-auth/providers/google"
-console.log(process.env.GOOGLE_CLIENT_SECRET);
 export const authOptions = {
     providers: [
       GoogleProvider({
-        clientId: "460214640878-303nkpsg2snldseccgcanai7poc8ogs5.apps.googleusercontent.com",
-        clientSecret: "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",
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOLGE_CLIENT_SECRET,
       }),
     ],
-    secret:"j2SwgPoucen7N6FpShIA+rGPE8gOCyfqacVEhYmQN/E="
+    secret:process.env.JWT_SECRET
   }
   export default NextAuth(authOptions)
