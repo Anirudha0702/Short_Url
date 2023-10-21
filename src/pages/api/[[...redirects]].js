8ce111baaa0fd6e1ef1d677361209925ff5d2120
Anirudha Pradhan <anirudhapradhan403@gmail.com>
2023-10-21 23:36:04 +0530

    redirects implement

---
+++ src/pages/api/[[...redirects]].js
@@ -0,0 +1,19 @@
+import {prisma} from "../../lib/prisma"
+export default async function handler(req, res) {
+  const {query:{redirects}}=req;
+  const[email,hashedUrl]=redirects;
+  console.log(email,hashedUrl)
+  try {
+    const _URL= await prisma.Url.findFirst(
+      {
+        where : {
+          hashedUrl : parseInt(hashedUrl,10),
+          user_email : email
+        }
+      })
+      await prisma.$disconnect()
+      return res.redirect(301,_URL.fullUrl);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+}
--- src/pages/api/generateHash.js
+++ src/pages/api/generateHash.js
@@ -1,7 +1,12 @@
+import CryptoJS from "crypto-js";
+function generateRandomNumber() {
+  return Math.floor(Math.random() * 9000000000) + 1000000000;
+}
 export default function hash(str){
+  // const uniqueIdentifier = generateRandomNumber();
     var hash = 0,
     i, chr;
-  if (str.length === 0) return hash;
+    if (str.length === 0) return hash;
   for (i = 0; i < str.length; i++) {
     chr = str.charCodeAt(i);
     hash = ((hash << 5) - hash) + chr;
