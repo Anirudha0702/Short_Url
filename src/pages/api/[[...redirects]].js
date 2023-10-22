8c863cd401fff4702273d90beffb7420df7737f1
Anirudha Pradhan <anirudhapradhan403@gmail.com>
2023-10-22 14:30:40 +0530

    dev pdt

--- src/pages/api/[[...redirects]].js
+++ src/pages/api/[[...redirects]].js
@@ -14,6 +14,7 @@ export default async function handler(req, res) {
       await prisma.$disconnect()
       return res.redirect(301,_URL.fullUrl);
   } catch (error) {
+    console.log(error)
     return res.status(500).json({ error: error.message });
   }
 }
